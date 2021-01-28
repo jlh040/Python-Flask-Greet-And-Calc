31394c2f469d2045bda39a9d0b9657c62fcb3b2d
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-28 14:15:04 -0700

    changed the comments

--- greet/app.py
+++ greet/app.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route('/welcome')
 def welcome():
-    """Return 'welcome'."""
+    """Return 'welcome.'"""
 
     return 'welcome'
 
 @app.route('/welcome/home')
 def welcome_home():
-    """Return 'welcome home'."""
+    """Return 'welcome home.'"""
 
     return 'welcome home'
 
 @app.route('/welcome/back')
 def welcome_back():
-    """Return 'welcome back'"""
-    
+    """Return 'welcome back.'"""
+
     return 'welcome back'
 
