caadf3a53d53d354cc3f91244ed18a341d97ff13
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-28 14:35:51 -0700

    added route for /add

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,12 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add():
+    a = int(request.args['a'])
+    b = int(request.args['b'])
+    sum = a + b
+
+    return str(sum)
