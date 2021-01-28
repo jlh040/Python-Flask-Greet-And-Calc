3edafa587bfd0e336f8d77548a3ebb002f6e7169
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-28 14:13:41 -0700

    made routes for three paths

---
+++ greet/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    """Return 'welcome'."""
+
+    return 'welcome'
+
+@app.route('/welcome/home')
+def welcome_home():
+    """Return 'welcome home'."""
+
+    return 'welcome home'
+
+@app.route('/welcome/back')
+def welcome_back():
+    """Return 'welcome back'"""
+    
+    return 'welcome back'
+
